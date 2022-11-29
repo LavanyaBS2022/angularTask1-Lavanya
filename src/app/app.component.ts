c35fdda682c3f3c5f39bd4d5af78f49250fcf6fc
LavanyaBS2022 <lavanyabs2022@gmail.com>
2022-11-29 16:02:01 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'login';
+    projectName="login";
+
+  userName='';
+  eMail='';
+  
 }
